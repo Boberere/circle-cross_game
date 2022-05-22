7344d235b9777ec10473ab599bff9c1857962051
Bartosz <bartoszbobrecki1@gmail.com>
2022-05-22 21:13:16 +0200

    js optimisation

--- js/script.js
+++ js/script.js
@@ -1,119 +1,78 @@
 // --------------------------------------------
-// -- SELECTORS
+// -- SELECTORS & VARIABLES
 // --------------------------------------------
 const boxes = document.querySelectorAll('.box')
 const gameInfo = document.querySelector('.game-info')
 const restartBtn = document.querySelector('.game-restart')
+const gameArea = document.querySelector('.game-area')
 // --------------------------------------------
 // -- FUNCTIONS
 // --------------------------------------------
-const setSymbols = e => {
-	//It checks that game areas are empty
-	if (!e.target.classList.contains(' e') && !e.target.classList.contains('cross')) {
-		turnChecker(e)
+const gameFunctions = e => {
+	// verifies that boxes are empty
+	if (!e.target.classList.contains('circle') && !e.target.classList.contains('cross')) {
+		// verifies whose turn is now.
+		setSymbol(e)
+		// changes players turn after setting symbol
 		turnHandle()
 	}
 }
-// This function changes turn after setting symbol
 const turnHandle = () => {
 	if (gameInfo.textContent === "It's circle's turn") {
 		gameInfo.textContent = "It's cross's turn"
 		return
-	}
-	if (gameInfo.textContent === "It's cross's turn") {
+	} else if (gameInfo.textContent === "It's cross's turn") {
 		gameInfo.textContent = "It's circle's turn"
 		return
 	}
 }
-//These two ifs in lower function, check that whose turn is.
-const turnChecker = e => {
+const setSymbol = e => {
 	if (gameInfo.textContent === "It's circle's turn") {
-		e.target.classList.toggle('circle')
+		e.target.classList.add('circle')
+	} else if (gameInfo.textContent === "It's cross's turn") {
+		e.target.classList.add('cross')
 	}
-	if (gameInfo.textContent === "It's cross's turn") {
-		e.target.classList.toggle('cross')
-	}
+	e.target.classList.add('symbol')
 }
-//Restart game
+// It is for restart game button
 const restartGame = () => {
 	gameInfo.textContent = "It's circle's turn"
 	boxes.forEach(box => {
-		box.classList.remove('circle', 'cross')
+		box.classList.remove('circle', 'cross', 'symbol')
 	})
+}
+// verifies conditions for displaying game result
+const resultInfo = () => {
+	// there is a need to transform nodeList to array in order to use every() method
+	const boxesArray = Array.prototype.slice.call(boxes)
+	const winCombinations = [
+		[boxesArray[0], boxesArray[1], boxesArray[2]],
+		[boxesArray[3], boxesArray[4], boxesArray[5]],
+		[boxesArray[6], boxesArray[7], boxesArray[8]],
+		[boxesArray[0], boxesArray[3], boxesArray[6]],
+		[boxesArray[1], boxesArray[4], boxesArray[7]],
+		[boxesArray[2], boxesArray[5], boxesArray[8]],
+		[boxesArray[2], boxesArray[4], boxesArray[6]],
+		[boxesArray[0], boxesArray[4], boxesArray[8]],
+	]
+	for (let combination of winCombinations) {
+		if (combination.every(x => x.classList.contains('circle'))) {
+			gameInfo.textContent = 'Circle is the winner!'
+			return
+		} else if (combination.every(x => x.classList.contains('cross'))) {
+			gameInfo.textContent = 'Cross is the winner!'
+			return
+		} 
+	}
+	if (boxesArray.every(x => x.classList.contains('symbol'))) {
+		gameInfo.textContent = 'Draw!'
+	}
 }
 // --------------------------------------------
 // -- LISTENERS
 // --------------------------------------------
-boxes.forEach(box => box.addEventListener('click', setSymbols))
 restartBtn.addEventListener('click', restartGame)
-
-const resultInfo = () => {
-	if (
-		(boxes[0].classList.contains('circle') &&
-			boxes[1].classList.contains('circle') &&
-			boxes[2].classList.contains('circle')) ||
-		(boxes[3].classList.contains('circle') &&
-			boxes[4].classList.contains('circle') &&
-			boxes[5].classList.contains('circle')) ||
-		(boxes[6].classList.contains('circle') &&
-			boxes[7].classList.contains('circle') &&
-			boxes[8].classList.contains('circle')) ||
-		(boxes[0].classList.contains('circle') &&
-			boxes[3].classList.contains('circle') &&
-			boxes[6].classList.contains('circle')) ||
-		(boxes[1].classList.contains('circle') &&
-			boxes[4].classList.contains('circle') &&
-			boxes[7].classList.contains('circle')) ||
-		(boxes[2].classList.contains('circle') &&
-			boxes[5].classList.contains('circle') &&
-			boxes[8].classList.contains('circle')) ||
-		(boxes[2].classList.contains('circle') &&
-			boxes[4].classList.contains('circle') &&
-			boxes[6].classList.contains('circle')) ||
-		(boxes[0].classList.contains('circle') &&
-			boxes[4].classList.contains('circle') &&
-			boxes[8].classList.contains('circle'))
-	) {
-		gameInfo.textContent = "Circle is the winner!"
-	} else if (
-		(boxes[0].classList.contains('cross') &&
-			boxes[1].classList.contains('cross') &&
-			boxes[2].classList.contains('cross')) ||
-		(boxes[3].classList.contains('cross') &&
-			boxes[4].classList.contains('cross') &&
-			boxes[5].classList.contains('cross')) ||
-		(boxes[6].classList.contains('cross') &&
-			boxes[7].classList.contains('cross') &&
-			boxes[8].classList.contains('cross')) ||
-		(boxes[0].classList.contains('cross') &&
-			boxes[3].classList.contains('cross') &&
-			boxes[6].classList.contains('cross')) ||
-		(boxes[1].classList.contains('cross') &&
-			boxes[4].classList.contains('cross') &&
-			boxes[7].classList.contains('cross')) ||
-		(boxes[2].classList.contains('cross') &&
-			boxes[5].classList.contains('cross') &&
-			boxes[8].classList.contains('cross')) ||
-		(boxes[2].classList.contains('cross') &&
-			boxes[4].classList.contains('cross') &&
-			boxes[6].classList.contains('cross')) ||
-		(boxes[0].classList.contains('cross') &&
-			boxes[4].classList.contains('cross') &&
-			boxes[8].classList.contains('cross'))
-	) {
-		gameInfo.textContent = "Cross is the winner!" 
-	} else if (
-		(boxes[0].classList.contains('circle') || boxes[0].classList.contains('cross')) &&
-		(boxes[1].classList.contains('circle') || boxes[1].classList.contains('cross')) &&
-		(boxes[2].classList.contains('circle') || boxes[2].classList.contains('cross')) &&
-		(boxes[3].classList.contains('circle') || boxes[3].classList.contains('cross')) &&
-		(boxes[4].classList.contains('circle') || boxes[4].classList.contains('cross')) &&
-		(boxes[5].classList.contains('circle') || boxes[5].classList.contains('cross')) &&
-		(boxes[6].classList.contains('circle') || boxes[6].classList.contains('cross')) &&
-		(boxes[7].classList.contains('circle') || boxes[7].classList.contains('cross')) &&
-		(boxes[8].classList.contains('circle') || boxes[8].classList.contains('cross'))
-	) {
-		gameInfo.textContent = 'Draw!'
-	}
-}
-setInterval(resultInfo, 100)
+boxes.forEach(box => {
+	box.addEventListener('click', gameFunctions)
+	box.addEventListener('click', resultInfo)
+})
